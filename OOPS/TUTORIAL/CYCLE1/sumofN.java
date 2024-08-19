08c66a9d6530fa1965653352ab4dd3f757021d7c
argatics <162816926+argatics@users.noreply.github.com>
2024-08-19 10:08:37 +0530

    Update sumofN.java

--- OOPS/TUTORIAL/CYCLE1/sumofN.java
+++ OOPS/TUTORIAL/CYCLE1/sumofN.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
 
+class sumofN {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += s.nextInt();
+        }
+
+        s.close();
+        System.out.println(sum);
+    }
+}
