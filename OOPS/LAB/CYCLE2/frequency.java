c44642d5ae774cbc610acc91de1a81a03235a5d5
argatics <162816926+argatics@users.noreply.github.com>
2024-08-19 09:42:43 +0530

    Update frequency.java

--- OOPS/LAB/CYCLE2/frequency.java
+++ OOPS/LAB/CYCLE2/frequency.java
@@ -1,1 +1,29 @@
-
+//To find the frequency of characters in a string
+//Albin Paul
+//Roll No:-6
+import java.util.*;
+class frequency
+{
+	public static void main(String args[])
+	{
+		Scanner s =new Scanner(System.in);
+		System.out.println("Enter a string");
+		String str=s.nextLine();
+		System.out.println("Enter the character");
+		String f=s.next();
+		char l=f.charAt(0);
+		int n= str.length();
+		int c=0;
+		for(int i=0;i<n;i++)
+		{
+			char m=str.charAt(i);
+			if (m==l)
+				c++;
+			
+			
+		}
+		System.out.println("the number of "+ f+" in "+str+ " is " +c);
+	}
+}
+			
+			
