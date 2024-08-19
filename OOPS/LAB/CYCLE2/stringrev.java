6ef28069be60d813514cddd19bd5b0aaec07f2c3
argatics <162816926+argatics@users.noreply.github.com>
2024-08-19 09:46:21 +0530

    Update stringrev.java

--- OOPS/LAB/CYCLE2/stringrev.java
+++ OOPS/LAB/CYCLE2/stringrev.java
@@ -1,1 +1,24 @@
-
+//To reverse a string
+//Albin Paul
+//Roll No:-6
+import java.util.*;
+class stringrev
+{
+	public static void main(String args[])
+	{
+	
+		Scanner s=new Scanner(System.in);
+		String word,rev="";
+		System.out.println("Enter the string: ");
+		word=s.nextLine();
+		int n=word.length();
+		for(int i=n-1;i>-1;i--)
+		{
+			rev=rev+word.charAt(i);
+		}
+		System.out.println("The Reverse of the "+word+" is "+rev);
+		s.close();
+	}
+}
+		
+			
